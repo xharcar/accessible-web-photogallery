8f1de0f801ba0bd9a88c01c4377138b9f7e5c371
xharcar <422714@mail.muni.cz>
2018-02-02 14:28:23 +0100

    Skeleton project with modules

---
+++ iface/src/test/java/cz/muni/fi/accessiblewebphotogallery/iface/IfaceApplicationTests.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.accessiblewebphotogallery.iface;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IfaceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
