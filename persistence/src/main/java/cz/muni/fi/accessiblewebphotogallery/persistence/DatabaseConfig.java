e1552381caac0ba72653267ab1724c9da84ae173
xharcar <422714@mail.muni.cz>
2018-05-17 04:39:42 +0200

    Externalised databse config works again - I have no idea what exactly fixed it but I'll take it; besides the visible changes I added persistence.properties to DatabaseConfig's context

--- persistence/src/main/java/cz/muni/fi/accessiblewebphotogallery/persistence/DatabaseConfig.java
+++ persistence/src/main/java/cz/muni/fi/accessiblewebphotogallery/persistence/DatabaseConfig.java
@@ -1,8 +1,12 @@
 package cz.muni.fi.accessiblewebphotogallery.persistence;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -15,27 +19,30 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
+import java.util.Objects;
 import java.util.Properties;
 
 @Component
 @Configuration
+@EnableConfigurationProperties
+@PropertySource("classpath:/persistence.properties")
 @EnableJpaRepositories
 @EnableTransactionManagement
 public class DatabaseConfig {
 
-    private final String packagesToScan = "cz.muni.fi.accessiblewebphotogallery.persistence.entity";
-    private final String dataSourceDriverClassName = "org.postgresql.Driver";
-    private final String dataSourceUrl = "jdbc:postgresql://localhost:5432/postgres";
-    private final String dataSourceUserName = "postgres";
-    private final String dataSourcePassword = "pgroot";
+    @Autowired
+    private Environment environment;
+
+
     private final String dialect = "org.hibernate.dialect.PostgreSQLDialect";
     private final String hbm2ddlAuto = "create-drop";
+
 
     @Bean
     public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
         LocalContainerEntityManagerFactoryBean rv = new LocalContainerEntityManagerFactoryBean();
         rv.setDataSource(dataSource());
-        rv.setPackagesToScan(packagesToScan);
+        rv.setPackagesToScan(Objects.requireNonNull(environment.getProperty("packages-to-scan")));
         HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         vendorAdapter.setGenerateDdl(true);
         rv.setJpaVendorAdapter(vendorAdapter);
@@ -46,10 +53,10 @@ public class DatabaseConfig {
     @Bean
     public DataSource dataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(dataSourceDriverClassName);
-        dataSource.setUrl(dataSourceUrl);
-        dataSource.setUsername(dataSourceUserName);
-        dataSource.setPassword(dataSourcePassword);
+        dataSource.setDriverClassName(Objects.requireNonNull(environment.getProperty("spring.datasource.driver-class-name")));
+        dataSource.setUrl(Objects.requireNonNull(environment.getProperty("spring.datasource.url")));
+        dataSource.setUsername(Objects.requireNonNull(environment.getProperty("spring.datasource.username")));
+        dataSource.setPassword(Objects.requireNonNull(environment.getProperty("spring.datasource.password")));
         return dataSource;
     }
 
@@ -67,8 +74,8 @@ public class DatabaseConfig {
 
     private Properties additionalProperties() {
         Properties properties = new Properties();
-        properties.setProperty("hibernate.hbm2ddl.auto", hbm2ddlAuto);
-        properties.setProperty("hibernate.dialect", dialect);
+        properties.setProperty("hibernate.hbm2ddl.auto", Objects.requireNonNull(environment.getProperty("hibernate.hbm2ddl.auto")));
+        properties.setProperty("hibernate.dialect", Objects.requireNonNull(environment.getProperty("hibernate.dialect")));
         return properties;
     }
 
